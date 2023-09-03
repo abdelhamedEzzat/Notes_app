f27f8919622078dd523ae6d3b4d6777da9de19e5
abdelhamid <amabdo275@gmail.com>
2023-09-03 16:00:50 +0300

    add bloc Provider in main view

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/Views/constant.dart';
 
 import 'package:notes_app/Views/notes_view.dart';
+import 'package:notes_app/cubit/cubit/add_note_cubit_cubit.dart';
 import 'package:notes_app/model/note_model.dart';
 
 void main() async {
@@ -20,7 +22,14 @@ class AppNote extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-      home: const NotesView(),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => AddNoteCubitCubit(),
+          ),
+        ],
+        child: const NotesView(),
+      ),
     );
   }
 }
